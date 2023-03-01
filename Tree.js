993f66f82e1ea18a1c48958bd7fe55702574c7c9
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-03-01 09:54:18 -0800

    Binary tree implemented, along with BFS

---
+++ Tree.js
@@ -0,0 +1,52 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.right = null;
+    this.left = null;
+  }
+}
+
+function addNode(root, value) {
+  if (value >= root.value) {
+    if (!root.right) {
+      root.right = new Node(value);
+      return;
+    }
+    addNode(root.right, value);
+  } else {
+    if (!root.left) {
+      root.left = new Node(value);
+      return;
+    }
+    addNode(root.left, value);
+  }
+}
+
+function BFS(root) {
+  let queue = [];
+  queue.push(root);
+
+  while (queue.length !== 0) {
+    let node = queue.shift();
+    console.log(node.value);
+
+    if (node.right) {
+      queue.push(node.right);
+    }
+    if (node.left) {
+      queue.push(node.left);
+    }
+  }
+}
+
+function DFS(root) {}
+
+let root = new Node(5);
+addNode(root, 7);
+addNode(root, 3);
+addNode(root, 9);
+addNode(root, 6);
+addNode(root, 4);
+addNode(root, 1);
+
+BFS(root);
