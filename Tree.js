faa44ca1554bf11d42c0fc3447dd25829a5b6ca0
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-03-01 10:05:27 -0800

    DFS preorder and postorder implemented

--- Tree.js
+++ Tree.js
@@ -39,14 +39,32 @@ function BFS(root) {
   }
 }
 
-function DFSPreOrder(root) {
+function DFSInOrder(root) {
   if (!root) {
     return;
   }
 
-  DFSPreOrder(root.left);
+  DFSInOrder(root.left);
   console.log(root.value);
+  DFSInOrder(root.right);
+}
+
+function DFSPreOrder(root) {
+  if (!root) {
+    return;
+  }
+  console.log(root.value);
+  DFSPreOrder(root.left);
   DFSPreOrder(root.right);
+}
+
+function DFSPostOrder(root) {
+  if (!root) {
+    return;
+  }
+  DFSPostOrder(root.left);
+  DFSPostOrder(root.right);
+  console.log(root.value);
 }
 
 let root = new Node(5);
@@ -57,4 +75,4 @@ addNode(root, 6);
 addNode(root, 4);
 addNode(root, 1);
 
-DFSPreOrder(root);
+DFSPostOrder(root);
