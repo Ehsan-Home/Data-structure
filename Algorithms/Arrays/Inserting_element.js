6ed59fa50a9795c16e25be5010f0a7a656bd2bdc
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-03-05 13:06:56 -0800

    add a condition to not insert the element if position is beyond array scope

--- Algorithms/Arrays/Inserting_element.js
+++ Algorithms/Arrays/Inserting_element.js
@@ -1,8 +1,9 @@
-// [1, 2, 3, 4, 5, 6]
-// arr.length = 6
-// i = 4
-// a[6] = a[5]
+// position > array.length
+// length = 6, pos = 6
 function insertElement(arr, element, position) {
+  if (position > arr.length) {
+    return -1;
+  }
   for (let i = arr.length - 1; i >= position; i--) {
     arr[i + 1] = arr[i];
   }
@@ -10,5 +11,5 @@ function insertElement(arr, element, position) {
 }
 
 const arr = [1, 2, 3, 4, 5, 6];
-insertElement(arr, 15, 2);
+insertElement(arr, 15, 7);
 console.log(arr);
